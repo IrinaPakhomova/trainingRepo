056c1cc4c36f82d507f614de4fc24d8ca3604e38
IrinaPakhomova <irinav.pakhomova@gmail.com>
2020-01-18 21:41:28 +0300

    Calculator (generic)

---
+++ TestProject/TestProject/Calculator/ICalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.Calculator
+{
+    interface ICalculator<T>
+    {
+        //Сложение
+        void Addition(T value1, T value2);
+
+        //Вычитание
+        void Subtraction(T value1, T value2);
+
+        //Умножение
+        void Multiplication(T value1, T value2);
+
+        //Деление
+        void Division(T value1, T value2);
+    }
+}
---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TestProject.Calculator;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ICalculator<int> calc = new Calculator<int>();
+            calc.Addition(5, 10);
+            calc.Subtraction(5, 10);
+            calc.Multiplication(5, 10);
+            calc.Division(5, 10);
+
+            Calculator<double> calc1 = new Calculator<double>();
+            calc1.Addition(10.3, 5);
+            calc1.Subtraction(10.3, 5);
+            calc1.Multiplication(10.3, 5);
+            calc1.Division(10.3, 5);
+
+            Console.WriteLine();
+            Calculator<char> calc2 = new Calculator<char>();
+            calc2.Addition('a', 'b');
+            calc2.Subtraction('l', 'm');
+            calc2.Multiplication('a', 'b');
+            calc2.Division('d', 'a');
+            
+            Calculator<string> calc3 = new Calculator<string>();
+            calc3.Addition("Happy ","new year");
+            calc3.Subtraction("Moskow", "Minsk");
+            calc3.Multiplication("Lena", "Michele");
+            calc3.Division("hdjhjhdkhd", "hdjhj");
+          
+           
+
+            Console.ReadLine();
+
+        }
+    }
+}
